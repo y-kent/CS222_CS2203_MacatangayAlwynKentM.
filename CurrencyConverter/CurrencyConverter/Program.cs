b55de89deae55da05f2f578ae937b9a015937e0c
Test User <test@example.com>
2026-02-10 15:26:42 +0800

    Add project files.

---
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Enter amount in USD: ");
+double usdAmount = double.Parse(Console.ReadLine());
+
+Console.Write("Enter exchange rate from USD to EUR: ");
+double exchangeRate = double.Parse(Console.ReadLine());
+
+double eurAmount = usdAmount * exchangeRate;
+
+Console.WriteLine("Amount in EUR: {0:F2}", eurAmount);
---
+++ InterestCalculator/InterestCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Enter principal amount: ");
+double Principal = double.Parse(Console.ReadLine());
+Console.Write("Enter rate of interest: ");
+double Rate = double.Parse(Console.ReadLine());
+Console.Write("Enter time in years: ");
+int Time = int.Parse(Console.ReadLine());
+double SimpleInterest = (Principal * Rate * Time) / 100;
+
+Console.WriteLine("Simple Interest: {0:F2}", SimpleInterest);
