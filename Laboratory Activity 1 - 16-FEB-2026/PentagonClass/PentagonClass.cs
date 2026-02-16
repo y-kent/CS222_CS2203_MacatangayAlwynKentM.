35011f0d1d9ca4201ae4b27ce73a4aa3a4fb63ae
Alwyn Kent M. Macatangay <191429373+y-kent@users.noreply.github.com>
2026-02-16 15:46:17 +0800

    Add files via upload

---
+++ Laboratory Activity 1 - 16-FEB-2026/PentagonClass/PentagonClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class PentagonC
+{
+    private double side;
+
+    public Pentagon(double side)
+    {
+        this.side = side;
+    }
+
+    public double GetPerimeter() 
+    {
+        return 5 * side;
+    }
+
+    public double GetArea()
+    {
+        return Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(side, 2) / 4;   
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Side length: " + side);
+        Console.WriteLine($"Perimeter: {GetPerimeter():F3}");
+        Console.WriteLine($"Area: {GetArea():F3}");
+    }
+
+    static void Main()
+    {
+        double side = 0; 
+        Pentagon pentagon1 = new Pentagon(side); 
+        Console.WriteLine("Pentagon p1");
+        Console.WriteLine("-------------------------");
+        pentagon1.Display();
+        Console.WriteLine();
+
+        side = 5;  
+        Pentagon pentagon2 = new Pentagon(side);
+        Console.WriteLine("Pentagon p2");
+        Console.WriteLine("-------------------------");
+        pentagon2.Display();
+    }
+}
